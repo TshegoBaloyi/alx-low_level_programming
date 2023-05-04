57c90a7032fafc0b14c7d484f3ee7d1d8722d790
TshegoBaloyi <tshegofatsosekhabi@gmail.com>
2023-05-04 15:08:26 -0700

    102 magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,10 +10,10 @@ int main(void)
   p = &n;
   /*
    * write your line of code here...
-   * Remember the following:
-   * - the use of a is prohibited
+   * Remember:
+   * - you are not allowed to use a
    * - you are not allowed to modify p
-   * - only write one statement
+   * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
   p[5] = 98;
