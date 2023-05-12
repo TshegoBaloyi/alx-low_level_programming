444de6cddcf492b06bb7e22581344df571c34ed5
TshegoBaloyi <tshegofatsosekhabi@gmail.com>
2023-05-12 13:31:57 -0700

    3 mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,19 +10,19 @@
 int _atoi(char *s)
 
 {
-	int i, d, n, len, f, digit;
+	int i, d, k, len, g, digit;
 
 	i = 0;
 	d = 0;
-	n = 0;
+	k = 0;
 	len = 0;
-	f = 0;
+	g = 0;
 	digit = 0;
 
 		while (s[len] != '\0')
 		len++;
 
-		while (i < len && f == 0)
+		while (i < len && g == 0)
 
 	{
 		if (s[i] == '-')
@@ -36,20 +36,20 @@ int _atoi(char *s)
 
 				digit = -digit;
 
-			n = n * 10 + digit;
-			f = 1;
+			k = k * 10 + digit;
+			g = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-				f = 0;
+				g = 0;
 		}
 		i++;
 	}
 
-	if (f == 0)
+	if (g == 0)
 	return (0);
 
 
-		return (n);
+		return (k);
 }
 
 /**
