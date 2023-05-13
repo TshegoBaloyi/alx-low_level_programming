cb107db970642acf112137f3682172fa3d0fe7a5
TshegoBaloyi <tshegofatsosekhabi@gmail.com>
2023-05-12 18:29:21 -0700

    libmy

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - a block of memory to be filled with a value
+ * @s: starting address of memory to be filed
+ * @b: a desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
